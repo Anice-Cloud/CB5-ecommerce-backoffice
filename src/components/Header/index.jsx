fb296e858cb07e2f05b507532557490d1d40cde9
Anna Sardone <aniceedmaster@gmail.com>
2023-01-23 12:38:10 +0100

    lezione

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className={styles.main}>Header</div>
+  )
+}
+
+export default Header
